1369d489a06775f31876c247edeb78a2da978580
崔健 <cuijian6@staff.sina.com.cn>
2018-11-23 13:57:39 +0800

    t

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const setting = JSON.parse(fs.readFileSync('./setting.json', 'utf8'));
 //   let file = path.resolve(__dirname, `${setting.filepath}`, `${fileName}`);
 //   res.sendFile(file);
 // });
+const root = path.resolve(__dirname, setting.filepath)
+app.use(express.static(root))
 
-app.use(express.static(path.resolve(__dirname, setting.filepath)))
 
 var server = app.listen(setting.port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('测试服务器开始运行 http://%s:%s', host, port);
+  console.log('根目录为: %s', root);
 });
